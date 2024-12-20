0ff5dd748a96096cfe305689bacb4bdfcd556c1a
abdelrahman <abdelrahmanabdellatif0@gmail.com>
2024-12-20 19:03:57 +0300

    Add basic functions and table structure in "main"

--- code.js
+++ code.js
@@ -1,1 +1,28 @@
+const completed = [];
+const reading = [];
+const planToRead = [];
 
+function Book(title, author, pageCount, raiting, status) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.raiting = raiting;
+  this.status = status;
+}
+
+function addBookToLibrary(title, author, pageCount, raiting, status) {
+
+    let book = new Book(title, author, pageCount, raiting, status);
+    
+  switch(status) {
+    case "completed":
+        completed.push(book)
+        break;
+    case "reading":
+        reading.push(book)
+        break;
+    case "plan to read":
+        planToRead.push(book)
+  }
+
+}
