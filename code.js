60282d27d4f45065c559f8a553c3fdc821646660
abdelrahman <abdelrahmanabdellatif0@gmail.com>
2024-12-21 19:39:38 +0300

    Add "new book" modal design

--- code.js
+++ code.js
@@ -21,8 +21,16 @@ function addBookToLibrary(title, author, pageCount, raiting, status) {
     case "reading":
         reading.push(book)
         break;
-    case "plan to read":
+    case "plan-to-read":
         planToRead.push(book)
   }
 
 }
+
+
+const addButton = document.querySelector(".add");
+const modal = document.querySelector("dialog")
+
+addButton.addEventListener("click", () => {
+    modal.showModal()
+})
